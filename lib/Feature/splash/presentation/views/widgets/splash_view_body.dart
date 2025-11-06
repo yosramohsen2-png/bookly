015ebdd81cadaee7d6e8895975ebca6e12b70f58
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-06 12:22:51 +0200

    change theme to dark

--- lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,12 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo)],
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 20),
+
+        const Text('Reed Free Books', textAlign: TextAlign.center),
+      ],
     );
   }
 }
