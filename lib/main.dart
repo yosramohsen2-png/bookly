2f8593619856d53fd73cbfdd6c1805a00b9e915e
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-06 11:16:11 +0200

    create splash view file

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/Feature/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
